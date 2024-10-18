27e25e88128dcb19ecf6315fb78d2373d7b7a971
WAFernandez99 <126583003+WAFernandez99@users.noreply.github.com>
2024-10-18 02:54:50 -0300

    Add files via upload

---
+++ prueba_waf.py
@@ -0,0 +1,93 @@
+#programa para la edad
+# edad= int(input ('Ingrese su edad'))
+# print ('El doble de su edad es', edad/2, 'años')
+
+#Nomenclatura de comillas y apostrofes.
+# print ('Hamlet dijo -"Hola" ')
+# print ("Llego el teniente O'Brian y esposo al sospechoso  ")
+
+# float convierte los numeros enteros, a numeros decimales.
+# Var1= float(input('Ingrese un numero'))
+# Var2= float(input('Ingrese un numero'))
+# print('Su resultado es', Var1+Var2, '.')
+
+
+# Var1= int(input('Ingrese un numero'))
+# Var2= int(input('Ingrese un numero'))
+#resultado= Var1+ Var2
+# print('Su resultado es',resultado)
+
+# Var1= float(input('Ingrese un numero'))
+# Var2= float(input('Ingrese un numero'))
+# print('Su resultado es', Var1+Var2, '.')
+
+# var1= int(input('Ingrese un numero'))
+# var2= int(input('Ingrese un numero'))
+# resultado= var1 < var2
+# print('Su resultado es',resultado)
+
+#Uso de todos los operadores
+# var1= int(input('Ingrese un numero'))
+# var2= int(input('Ingrese un numero'))
+# mult= var1 * var2
+# div= var1 / var2
+# dive= var1 // var2
+# resto= var1 % var2
+# potencia= var1 ** var2
+# menos= var1 - var2
+# print('Los resultado son=', mult, 'Multiplicacion' , div, 'Division'  ,dive, 'Division entera' ,resto, 'Resto' ,potencia, 'Potencia' ,menos,'Resta')
+
+#multiplicar texto por numero que devuelve el mismo por n veces
+# var1= input('Ingrese un texto')
+# var2= int(input('Ingrese un numero'))
+# resultado= var1 * var2
+# print('El resultado es' ,resultado,)
+
+#Uso de propiedades del texto
+# texto= "holA coMo estan?"
+# print( texto.upper())
+# print( texto.lower())
+# minúsculas= texto.lower
+# print(minúsculas)
+# print(len(texto))
+# cantidad=texto.count ("a")
+# print(cantidad)
+
+#el str convierte en texto el primer valor.
+# num= 45
+# t= str (num)
+# print(t * 3)
+
+#La numeracion arranca desde 0.
+# texto= "Hola como estas?"
+# print(texto[3])
+# #Posiciona entre la a y c.
+# print(texto[3:6])
+# #Ambos se posicionan en a hasta el final.
+# print(texto[3:len(texto)])
+# print(texto[3:])
+# #Ambos se posicionan desde el inicio hasta el segundo termino.
+# print(texto[0:3])
+# print(texto[:3])
+
+# #Metodo slices
+# frase=input("Ingrese una frase")
+# palabra=input("Ingrese un palabra a eliminar")
+# #find busca desde donde empieza.
+# posicion=frase.find(palabra)
+# print("posicion=",posicion,)
+# #Desde el inicio hasta la primera posicion de la palabra.
+# parte1= frase[:posicion]
+# #Desde la posicion, se le suman los caracteres de toda la palabra, hasta el final.
+# parte2=frase[posicion + len(palabra):]
+# frase= parte1 + parte2
+# print(frase)
+
+
+
+
+
+
+
+
+
